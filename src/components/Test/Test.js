389415ab6f3cccabe62449e83b1fd44fb7c610c8
Oh YeJun <oagree0123@naver.com>
2022-03-03 13:45:47 +0900

    🎉 [오예준] 시작 커밋

---
+++ src/components/Test/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TestWrap } from './style';
+
+const Test = () => {
+  return (
+    <>
+      <TestWrap />
+    </>
+  );
+};
+
+export default Test;
---
+++ src/components/Test/style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const TestWrap = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #000;
+`;
