53b063f8ef665497adbc17589aebbfaaf5b8e3ca
Oh YeJun <oagree0123@naver.com>
2022-03-30 11:50:41 +0900

    🐛 [오예준] 배너 클릭시 브라우저 새 탭 생성

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -22,15 +22,15 @@ const Slide = (props) => {
   return (
     <Wrap>
       <Slider {...settings}>
-        <CardLink href="https://docs.google.com/forms/d/1UbMnKzM9fC6Wp_tuzpZ5_IdqC4o5zfuzdIOcnDqzIK8/viewform?edit_requested=true">
+        <CardLink target='_blank' href="https://docs.google.com/forms/d/1UbMnKzM9fC6Wp_tuzpZ5_IdqC4o5zfuzdIOcnDqzIK8/viewform?edit_requested=true">
           <Card src={bug} />
         </CardLink>
 
-        <CardLink href="https://docs.google.com/forms/d/1AM411fsh0aGXFYL3JygTiYlmO2z766eGEEVwk23ABlI/viewform?edit_requested=true">
+        <CardLink target='_blank' href="https://docs.google.com/forms/d/1AM411fsh0aGXFYL3JygTiYlmO2z766eGEEVwk23ABlI/viewform?edit_requested=true">
           <Card src={survey} />
         </CardLink>
 
-        <CardLink href="https://docs.google.com/forms/d/1p6y0AgdW80phMLgN9vfQ_hWu2IIxJQ_vIX9yQQtpcJU/viewform?edit_requested=true">
+        <CardLink target='_blank' href="https://docs.google.com/forms/d/1p6y0AgdW80phMLgN9vfQ_hWu2IIxJQ_vIX9yQQtpcJU/viewform?edit_requested=true">
           <Card src={ranking} />
         </CardLink>
       </Slider>
